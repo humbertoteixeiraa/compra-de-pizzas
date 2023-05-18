9f6905163555b43194b958d067c8720174a0ad12
Humberto Teixeira <humberto.quim@gmail.com>
2023-05-18 00:18:07 -0300

    Updates of the day 05.18.2023

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+pizzaJson.map(function(item, index) {
+
+    //Copying the complete structure with .cloneNode().
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+
+
+});
