0348f3f4bf5c7311686adedcf63583f8d3b01505
Humberto Teixeira <humberto.quim@gmail.com>
2023-05-20 15:43:36 -0300

    Updates of the day 05.20.2023

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -6,7 +6,7 @@ let pizzaJson = [
         img:'assets/images/pizza1.png', 
         price:20.19, 
         sizes:['100g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Molho de tomate e mussarela (2 camadas)'
     },
 
     {
@@ -15,7 +15,7 @@ let pizzaJson = [
         img:'assets/images/pizza2.png', 
         price:18.00, 
         sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Molho de tomate, mussarela e calabresa'
     },
 
     {
@@ -24,7 +24,7 @@ let pizzaJson = [
         img:'assets/images/pizza3.png', 
         price:17.45, 
         sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Molho de tomate, mussarela, provolone, parmesão e catupiry'
     },
 
     {
@@ -33,7 +33,7 @@ let pizzaJson = [
         img:'assets/images/pizza4.png', 
         price:19.77, 
         sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Molho de tomate, mussarela, bacon, calabresa, ovos e cebola'
     },
 
     {
@@ -41,7 +41,7 @@ let pizzaJson = [
         name:'Sorvete', 
         img:'assets/images/pizza5.png', 
         price:21.43, sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Chocolate ao leite e bolas de sorvete'
     },
 
     {
@@ -50,7 +50,7 @@ let pizzaJson = [
         img:'assets/images/pizza6.png', 
         price:18.55, 
         sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Molho de tomate, presunto, milho, ervilha, calabresa, tomates, ovos, azeitonas e mussarela (2 camadas)'
     },
 
     {
@@ -59,6 +59,6 @@ let pizzaJson = [
         img:'assets/images/pizza7.png', 
         price:22.36, 
         sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        description:'Creme de leite, chocolate ao leite e fios de bisnaga'
     }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,27 +9,41 @@ const cs = function(el) {
 
 /**/
 
-//Calling the .map() function on the array of ''pizzaJson'' objects.
+//Calling the .map() function on the array of 'pizzaJson' objects.
 //'item' is each array object.
 pizzaJson.map(function(item, index) {
 
     //Copying the complete structure with .cloneNode().
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
-    //Adding pizza images
+    //Adding 'data-key' attribute in the 'pizaItem' class.
+    pizzaItem.setAttribute('data-key', index);
+    //Adding pizza images.
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    //Replacing pizza price
+    //Replacing pizza price.
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    //Replacing pizza names
+    //Replacing pizza names.
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    //Replacing pizza description
+    //Replacing pizza description.
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
-    //
+    //Executing function 'function(e) { }' when clicking <a> tag.
     pizzaItem.querySelector('a').addEventListener('click', function(e) {
         e.preventDefault();
 
-        //Changing the CSS to show the content of '.pizzaWindowArea' and setting the animation
+        //'Event.target' references the element that fired the event.
+        //'Method.closest()' returns the closest ancestor, relative to the current element, that has the provided selector as a parameter.
+        /* 
+         - ATTENTION! -> Seria possível utilizar 'let key = index', já que o valor de 'key' é o mesmo de 'index'. NO ENTANTO, caso seja necessário refatorar esse código e extrair a função adicionada ao .addEventListener() para fora de.map(), seria perfeitamente possível já que ela não depende de informações de fora dela.
+        */
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        //Filling in the information of <div class="pizzaInfo">.
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaBig img').src = pizzaJson[key].img;
+
+        //Changing the CSS to show the content of '.pizzaWindowArea' and setting the animation.
         c('.pizzaWindowArea').style.display = 'flex';
         c('.pizzaWindowArea').style.opacity = 0;
         setTimeout(function() {
